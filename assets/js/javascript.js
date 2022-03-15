bd68a06673d267ea1a4b927859d976b0f9f087a9
Laegon <stucchi19@gmail.com>
2022-03-15 20:42:21 +0100

    comparazione risultato

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,53 +1,58 @@
-var total_caracteres = 6;
-var total_cliques = 0;
+var total_characters = 10;
+var total_clicks = 0;
 var password="111111";
 
+function calculateResult(n){
+		if (n < 2) {
+		  return n;
+		}
+		return calculateResult(n - 1) + calculateResult(n - 2);
+}
+
 function makeButtons(){	
-let temp_= "";
-let teclas = [1,2,3,4,5,6,7,8,9,0];
-for (var i = 0; i<teclas.length-1; i++) {
-temp_+= '<button type="button" value="' + teclas[i] + '" onclick="clickNumber(this.value)">' + teclas[i] + '</button>';
- document.getElementById("keys").innerHTML = temp_;
-};
+	let temp_= "";
+	let teclas = [1,2,3,4,5,6,7,8,9,0];
+	for (var i = 0; i<teclas.length-1; i++) {
+	temp_+= '<button type="button" value="' + teclas[i] + '" onclick="clickNumber(this.value)">' + teclas[i] + '</button>';
+	document.getElementById("keys").innerHTML = temp_;
+	};
 
-temp_ = '<button class="del" type="button" onclick="clickClear()">DEL</button>';
-document.getElementById("keys").innerHTML += temp_;
-  
-temp_ = '<button type="button" value="' + teclas[9] + '" onclick="clickNumber(this.value)">' + teclas[9] + '</button>';
-document.getElementById("keys").innerHTML += temp_;
+	temp_ = '<button class="del" type="button" onclick="clickClear()">DEL</button>';
+	document.getElementById("keys").innerHTML += temp_;
+	
+	temp_ = '<button type="button" value="' + teclas[9] + '" onclick="clickNumber(this.value)">' + teclas[9] + '</button>';
+	document.getElementById("keys").innerHTML += temp_;
 
-temp_ = '<button class="confirm" type="button" onclick="clickOk()">OK</button>';
-document.getElementById("keys").innerHTML += temp_;
+	temp_ = '<button class="confirm" type="button" onclick="clickOk()">OK</button>';
+	document.getElementById("keys").innerHTML += temp_;
 
 }
 
 makeButtons();
 
 	function clickNumber(str){
-		if (total_caracteres>total_cliques){
+		if (total_characters>total_clicks){
 			document.getElementById("newpassword").value += str;
-			total_cliques ++;
-
-
+			total_clicks ++;
 		}
 	}
 
 function clickClear(){
-document.getElementById("newpassword").value = "";
-total_cliques = 0;
-
+	document.getElementById("newpassword").value = "";
+	total_clicks = 0;
 }
 
 function clickOk(){
-if(total_cliques == total_caracteres){
-password = document.getElementById("newpassword").value;
-console.log(password);
-
-document.getElementById("newpassword").value = "";
-total_cliques=0;
-} else{
-	console.log("wrong password");
-	alert("Wrong Password");
-}
+	password = document.getElementById("newpassword").value;
+	document.getElementById("newpassword").value = "";
+	total_clicks=0;
+	console.log(password);
+	console.log(calculateResult(24))
+	if(calculateResult(24) === parseInt(password)){
+		alert("Correct");
+	}
+	else{
+		alert("Wrong password");
+	}
 }
 
